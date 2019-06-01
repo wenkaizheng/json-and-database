b56f7fd152e224098e35f29e8a13034bee9b680e
wenkaizheng <31551859+wenkaizheng@users.noreply.github.com>
2019-06-01 15:18:56 +0800

    Add files via upload
    add basic project

---
+++ App.java
@@ -0,0 +1,59 @@
+package database.db1;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import org.json.JSONArray;
+import org.json.JSONObject;
+public class App {
+  
+
+  public static String getText(String url) throws Exception {
+    URL website = new URL(url);
+    URLConnection connection = website.openConnection();
+    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF8"));
+    StringBuilder response = new StringBuilder();
+    String inputLine;
+    while ((inputLine = in.readLine()) != null)
+      response.append(inputLine);
+
+    in.close();
+
+    return response.toString();
+  }
+
+  public static void main(String[] args) throws Exception {
+
+    System.setProperty("http.agent", "Chrome");
+    JSONArray jsonArray = new JSONArray(getText("https://api.warframestat.us/weapons"));
+    System.out.printf("Welcome to star war weapon search page there are totally %d weapon to choose\n",
+        jsonArray.length());
+   // Scanner read = new Scanner(System.in);
+    //String Name = read.nextLine();
+    // iterate loop
+    db database =new db();
+    if(database.create()==1) {
+    	return ;
+    }
+    for (int i = 0; i < jsonArray.length(); i++) {
+
+        JSONObject obj = jsonArray.getJSONObject(i);
+        String name = obj.getString("name");
+      
+        int damage = obj.getInt("totalDamage");
+        double persecond = obj.getDouble("secondsPerShot");
+        int accuracy = obj.getInt("accuracy");
+        String des = obj.getString("description");
+
+        weapon init =new weapon(name, damage, persecond, accuracy, des);
+       // System.out.println(init);
+        
+        database.insert(init,i);
+        
+        
+        
+      
+    }
+  }
+
+}
---
+++ db.java
@@ -0,0 +1,128 @@
+package database.db1;
+
+import java.sql.*;
+
+public class db {
+	// JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://127.0.0.1/";
+	// "jdbc:mysql://127.0.0.1/wineships";
+
+	// Database credentials
+	static final String USER = "root";
+	static final String PASS = "123456";
+	public Connection conn = null;
+	public Statement stmt = null;
+
+	public void insert(weapon w,int num) {
+		/**
+		 * String sql = "INSERT INTO Registration " + "VALUES (100, 'Zara', 'Ali', 18)";
+		 * stmt.executeUpdate(sql);
+		 */
+		String name = w.name;
+		int dam = w.damage;
+		double shot = w.persecond;
+		int acc = w.accuracy;
+		String des = w.des;
+		String sql = "USE wuqiku";
+		try {
+			stmt.executeUpdate(sql);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		// sql = "INSERT INTO REGISTRATION VALUES (" + "'" + name + "', " + dam + ", " + shot + ", " + acc + ", " + "'"
+			//	+ des + "')" ;
+		//INSERT INTO exampleTbl VALUES('he doesn''t work for me')
+		if(des.contains("'")){
+			// he does't like it -> he does 't like it 
+			des=des.replace("'", "''");
+	    }
+		sql =String.format("INSERT INTO Registration VALUES (%d, '%s', %d, %f, %d, '%s')", num,name,dam,shot,acc,des);
+		System.out.println(sql);
+		if (stmt != null) {
+			try {
+				stmt.executeUpdate(sql);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	public int create() {
+		
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			ResultSet resultSet = conn.getMetaData().getCatalogs();
+
+			// iterate each catalog in the ResultSet
+			
+			while (resultSet.next()) {
+				// Get the database name, which is at position 1
+				String databaseName = resultSet.getString(1);
+				System.out.println(databaseName);
+				if (databaseName.equals("wuqiku")) {
+
+					System.out.println("This database is already exist");
+					resultSet.close();
+					return 1;
+				}
+
+			}
+			resultSet.close();
+			
+
+			// STEP 4: Execute a query
+			System.out.println("Creating database...");
+			
+			stmt = conn.createStatement();
+            
+			String sql = "CREATE DATABASE wuqiku";
+			stmt.executeUpdate(sql);
+			
+			sql = "USE wuqiku";
+			stmt.executeUpdate(sql);
+			
+			// String column =new String("CREATE TABLE 'quotations' ( 'id' int(20) NOT NULL
+			// AUTO INCREMENT,'added' datetime DEFAULT NULL,"+
+			// "'name' varchar(2000) DEFAULT NULL,'damage' int(11) DEFAULT NULL,'persecond'
+			// DOUBLE(100) DEFAULT NULL,"+
+			// "'acc' int(11) DEFAULT NULL,'des' varchar(2000) DEFAULT NULL,PRIMARY KEY
+			// (`id`))");
+			/**
+			String column = "CREATE TABLE REGISTRATION " + "(id INTEGER not NULL , "
+					+ " added datetime, " + " name VARCHAR(255), "
+					+ " damage int(200), " + " persecond DOUBLE(100), "
+					+ " acc int(200), " + " des VARCHAR(255), "
+                    + " PRIMARY KEY ( id ))";
+                    **/
+			 
+			String column = "CREATE TABLE REGISTRATION " +
+	                   "(id INTEGER not NULL, " +
+	                   " name VARCHAR(255), " + 
+	                   " damage INTEGER, " + 
+	                   " persecond DOUBLE, " + 
+	                   " acc INTEGER, "+
+	                   " de VARCHAR(1024), "+
+	                   " PRIMARY KEY ( id ))"; 
+			stmt.executeUpdate(column);
+
+			System.out.println("Database created successfully...");
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		}
+		System.out.println("Goodbye!");
+		return 0;
+	}// end main
+}// end JDBCExample
